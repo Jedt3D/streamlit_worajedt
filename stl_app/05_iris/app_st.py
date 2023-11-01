f4d26d74e3ad5425150e07ce673525a568d8ff65
sutthipong_sangram <66056084@kmitl.ac.th>
2023-11-01 18:39:31 +0700

    Scatter plot & Predict iris

--- stl_app/05_iris/app_st.py
+++ stl_app/05_iris/app_st.py
@@ -7,56 +7,68 @@ from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
-st.title('iris Classifier')
-st.write("This app uses 4 inputs to predict the variety of iris")
+st.title("Iris")
+st.title("Iris Dataset")
+st.markdown('สร้าง `scatter plot` แสดงผลข้อมูล **Iris dataset**')
 
-iris_file = st.file_uploader('Upload your own iris data')
-
-# if iris_file is None:
-#     rf_pickle = open('random_forest_iris.pickle', 'rb')
-#     map_pickle = open('output_iris.pickle', 'rb')
-#
-#     rfc = pickle.load(rf_pickle)
-#     unique_iris_mapping = pickle.load(map_pickle)
-#
-#     rf_pickle.close()
+choices = ['sepal.length','sepal.width','petal.length','petal.width']
+iris_file = None
+iris_file = st.file_uploader("Choose a CSV file", accept_multiple_files=False)
 
 if iris_file is not None:
     iris_df = pd.read_csv(iris_file)
-    iris_df = iris_df.dropna()
-
-    output = iris_df['variety']
-    features = iris_df[['sepal.length', 'sepal.width', 'petal.length','petal.width']]
-
-    features = pd.get_dummies(features)
-
-    output, unique_iris_mapping = pd.factorize(output)
-
-    x_train, x_test, y_train, y_test = train_test_split(
-        features, output, test_size=.8)
-
-    rfc = RandomForestClassifier(random_state=15)
-    rfc.fit(x_train, y_train)
-
-    y_pred = rfc.predict(x_test)
-
-    score = round(accuracy_score(y_pred, y_test), 2)
-
-    st.write('We trained a Random Forest model on these data,'
-             ' it has a score of {}! Use the '
-             'inputs below to try out the model.'.format(score))
 else:
     st.stop()
 
+st.subheader('ข้อมูลตัวอย่าง')
+st.write(iris_df.head(5))
+
+selected_x_var = st.selectbox('What do you want to select x?',(choices))
+selected_y_var = st.selectbox('What do you want to select y?',(choices))
+
+st.subheader('แสดงผลข้อมูล')
+sns.set_style('darkgrid')
+markers = {"Setosa": "v", "Virginica": "s", "Versicolor": 'o'}
+fig, ax = plt.subplots()
+ax = sns.scatterplot(data=iris_df,
+                     x=selected_x_var, y=selected_y_var,
+                     hue='variety', markers=markers, style='variety')
+plt.xlabel(selected_x_var)
+plt.ylabel(selected_y_var)
+plt.title("iris Data")
+st.pyplot(fig)
+
+st.title('iris Classifier')
+st.write("This app uses 4 inputs to predict the variety of iris")
+
+iris_df = iris_df.dropna()
+
+output = iris_df['variety']
+features = iris_df[['sepal.length', 'sepal.width', 'petal.length','petal.width']]
+
+features = pd.get_dummies(features)
+
+output, unique_iris_mapping = pd.factorize(output)
+
+x_train, x_test, y_train, y_test = train_test_split(features, output, test_size=.8)
+
+rfc = RandomForestClassifier(random_state=15)
+rfc.fit(x_train, y_train)
+
+y_pred = rfc.predict(x_test)
+
+score = round(accuracy_score(y_pred, y_test), 2)
+
+st.write('We trained a Random Forest model on these data,'
+             ' it has a score of {}! Use the '
+             'inputs below to try out the model.'.format(score))
+
+
 with st.form('user_inputs'):
-    sepal_length = st.number_input(
-        'sepal.length', min_value=0, value=10)
-    sepal_width = st.number_input(
-        'sepal.width', min_value=0, value=10)
-    petal_length = st.number_input(
-        'petal.length', min_value=0, value=10)
-    petal_width = st.number_input(
-        'petal.width', min_value=0, value=10)
+    sepal_length = st.number_input('sepal.length', min_value=0.0,max_value=10.0, value=0.0)
+    sepal_width  = st.number_input('sepal.width' , min_value=0.0,max_value=10.0, value=0.0)
+    petal_length = st.number_input('petal.length', min_value=0.0,max_value=10.0, value=0.0)
+    petal_width  = st.number_input('petal.width' , min_value=0.0,max_value=5.0, value=0.0)
     st.form_submit_button()
 
 new_prediction = rfc.predict([[sepal_length, sepal_width, petal_length,petal_width]])
@@ -64,3 +76,4 @@ new_prediction = rfc.predict([[sepal_length, sepal_width, petal_length,petal_wid
 prediction_species = unique_iris_mapping[new_prediction][0]
 
 st.write('We predict variety is the {}'.format(prediction_species))
+
