ce1a25906f67deccbc1cdf99848b27608e02cfcb
sutthipong_sangram <66056084@kmitl.ac.th>
2023-11-01 21:00:12 +0700

    streamlit 02 update Tab

--- streamlit02/app.py
+++ streamlit02/app.py
@@ -2,6 +2,20 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from st_pages import Page, show_pages
+
+st.set_page_config(layout='wide')
+
+# ----- Pages -----
+show_pages(
+    [
+        Page('app.py', 'Home',''),
+        Page('pages/tab.py', 'Layout',''),
+        Page('pages/map.py', 'Map','')
+    ]
+)
+
+
 st.markdown('สวัสดี! *Streamlit*')
 st.title('Layout and Decoration')
 st.write("""
