63531c0bc131ab510191441237cad72ef81a330e
sutthipong_sangram <66056084@kmitl.ac.th>
2023-11-01 21:56:15 +0700

    streamlit 02 update filter

--- streamlit02/app.py
+++ streamlit02/app.py
@@ -9,8 +9,8 @@ st.set_page_config(layout='wide')
 # Create pages
 show_pages(
     [
-        Page('app.py', 'Home','🏠')
-        # Page('pages/tab.py', 'Layout','📈'),
+        Page('app.py', 'Home','🏠'),
+        Page('pages/tab.py', 'Layout','📈')
         # Page('pages/map.py', 'Map','🌍')
     ]
 )
@@ -21,9 +21,9 @@ st.title('Layout and Decoration')
 st.write("""
  เราจะลองทำ San Francisco Dataset กันดู
 """)
+st.divider()
 
-
-# Read data & filter
+#---------------Read data & filter-------------------------
 tree_df = pd.read_csv('trees.csv')
 
 owners = st.sidebar.multiselect("Owner filter", tree_df['caretaker'].unique())
@@ -35,9 +35,9 @@ if owners != []:
 tree_df = tree_df.query(query)
 df_dbh_grouped = pd.DataFrame(tree_df.groupby(['dbh']).count()['tree_id'])
 df_dbh_grouped.columns = ['tree_count']
-st.divider()
 
-# ------ Columns ------
+
+#---------------Create column-------------------------
 col1, col2, col3 = st.columns(3)
 with col1:
     st.write('Column1')
@@ -46,6 +46,19 @@ with col2:
     st.write('Column2')
     st.bar_chart(df_dbh_grouped)
 with col3:
+    st.write('Column3')
+    st.area_chart(df_dbh_grouped)
+st.divider()
+
+---------------Create tab-------------------------
+tab1,tab2,tab3 = st.tabs(["Line", "Bar", "Area"])
+with tab1:
+    st.write('Column1')
+    st.line_chart(df_dbh_grouped)
+with tab2:
+    st.write('Column2')
+    st.bar_chart(df_dbh_grouped)
+with tab3:
     st.write('Column3')
     st.area_chart(df_dbh_grouped)
 st.divider()
