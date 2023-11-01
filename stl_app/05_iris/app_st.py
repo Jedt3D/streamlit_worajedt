63f6b04d3cd365060a10b177887bd2774198a461
sutthipong_sangram <66056084@kmitl.ac.th>
2023-11-01 18:12:40 +0700

    Predict iris

--- stl_app/05_iris/app_st.py
+++ stl_app/05_iris/app_st.py
@@ -7,26 +7,26 @@ from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
-st.title('Penguin Classifier')
-st.write("This app uses 4 inputs to predict the species of iris")
+st.title('iris Classifier')
+st.write("This app uses 4 inputs to predict the variety of iris")
 
-iris_file = st.file_uploader('Upload your own penguin data')
+iris_file = st.file_uploader('Upload your own iris data')
 
-if iris_file is None:
-    rf_pickle = open('random_forest_iris.pickle', 'rb')
-    map_pickle = open('output_iris.pickle', 'rb')
+# if iris_file is None:
+#     rf_pickle = open('random_forest_iris.pickle', 'rb')
+#     map_pickle = open('output_iris.pickle', 'rb')
+#
+#     rfc = pickle.load(rf_pickle)
+#     unique_iris_mapping = pickle.load(map_pickle)
+#
+#     rf_pickle.close()
 
-    rfc = pickle.load(rf_pickle)
-    unique_iris_mapping = pickle.load(map_pickle)
-
-    rf_pickle.close()
-else:
+if iris_file is not None:
     iris_df = pd.read_csv(iris_file)
     iris_df = iris_df.dropna()
 
     output = iris_df['variety']
-    features = iris_df[['sepal.length', 'sepal.width', 'petal.length',
-                           'petal.width']]
+    features = iris_df[['sepal.length', 'sepal.width', 'petal.length','petal.width']]
 
     features = pd.get_dummies(features)
 
@@ -45,6 +45,8 @@ else:
     st.write('We trained a Random Forest model on these data,'
              ' it has a score of {}! Use the '
              'inputs below to try out the model.'.format(score))
+else:
+    st.stop()
 
 with st.form('user_inputs'):
     sepal_length = st.number_input(
@@ -57,7 +59,8 @@ with st.form('user_inputs'):
         'petal.width', min_value=0, value=10)
     st.form_submit_button()
 
-new_prediction = rfc.predict([[sepal_length, sepal_width, petal_length,
-                               petal_width]])
+new_prediction = rfc.predict([[sepal_length, sepal_width, petal_length,petal_width]])
+
 prediction_species = unique_iris_mapping[new_prediction][0]
-st.write('We predict your penguin is of the {} species'.format(prediction_species))
+
+st.write('We predict variety is the {}'.format(prediction_species))
