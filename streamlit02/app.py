bd64f39a0287b0e9d0d3100086c7cdadf28a71ff
sutthipong_sangram <66056084@kmitl.ac.th>
2023-11-01 21:41:30 +0700

    streamlit 02 update filter

--- streamlit02/app.py
+++ streamlit02/app.py
@@ -26,14 +26,26 @@ st.write("""
 # Read data & filter
 tree_df = pd.read_csv('trees.csv')
 
-owners = st.sidebar.multiselect("Caretaker filter", tree_df['caretaker'].unique())
-# query = '(index == index or index != index)'
-#
-# if owners != []:
-#     query += ' and caretaker in @owners'
-#
-# # tree_df = tree_df.query(query)
-# # df_dbh_grouped = pd.DataFrame(tree_df.groupby(['dbh']).count()['tree_id'])
-# # df_dbh_grouped.columns = ['tree_count']
-# # st.divider()
+owners = st.sidebar.multiselect("Owner filter", tree_df['caretaker'].unique())
+query = '(index == index or index != index)'
 
+if owners != []:
+    query += ' and caretaker in @owners'
+
+tree_df = tree_df.query(query)
+df_dbh_grouped = pd.DataFrame(tree_df.groupby(['dbh']).count()['tree_id'])
+df_dbh_grouped.columns = ['tree_count']
+st.divider()
+
+# ------ Columns ------
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.write('Column1')
+    st.line_chart(df_dbh_grouped)
+with col2:
+    st.write('Column2')
+    st.bar_chart(df_dbh_grouped)
+with col3:
+    st.write('Column3')
+    st.area_chart(df_dbh_grouped)
+st.divider()
