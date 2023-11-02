cf47e2bf83de234fd83b2fff40613a48db64c013
sutthipong_sangram <66056084@kmitl.ac.th>
2023-11-02 16:31:41 +0700

    streamlit 02 change pages

--- streamlit02/app.py
+++ streamlit02/app.py
@@ -5,51 +5,122 @@ import matplotlib.pyplot as plt
 from st_pages import Page, show_pages, add_page_title
 import time
 
-#---------------Create page-------------------------
-#st.set_page_config(layout='wide')
-add_page_title()
+# #---------------Create page-------------------------
+#
+#
+# show_pages([
+#         Page('app.py', 'Home','🏠'),
+#         Page('tab.py', 'Tab','📈')
+#         # Page('pages/map.py', 'Map','🌍')
+#           ])
+#
+# st.markdown('สวัสดี! *Streamlit*')
+# st.title('Layout and Decoration')
+# st.write("""
+#  เราจะลองทำ San Francisco Dataset กันดู
+# """)
+#
+#
+#
+# #---------------Read data & filter-------------------------
+# tree_df = pd.read_csv('trees.csv')
+# owners = st.sidebar.multiselect("Owner filter", tree_df['caretaker'].unique())
+# st.sidebar.success("Select filter above.")
+#
+# query = '(index == index or index != index)'
+# if owners != []:
+#     query += ' and caretaker in @owners'
+#
+# tree_df = tree_df.query(query)
+# df_dbh_grouped = pd.DataFrame(tree_df.groupby(['dbh']).count()['tree_id'])
+# df_dbh_grouped.columns = ['tree_count']
+#
+# #---------------Create column-------------------------
+#
+# col1, col2, col3 = st.columns(3)
+# with col1:
+#     st.write('Column1')
+#     st.line_chart(df_dbh_grouped)
+# with col2:
+#     st.write('Column2')
+#     st.bar_chart(df_dbh_grouped)
+# with col3:
+#     st.write('Column3')
+#     st.area_chart(df_dbh_grouped)
+# st.divider()
 
-show_pages([
-        Page('app.py', 'Home','🏠'),
-        Page('tab.py', 'Tab','📈')
-        # Page('pages/map.py', 'Map','🌍')
-          ])
+# Read data & filter
+def read_data():
+   tree_df = pd.read_csv('trees.csv')
+   owners = st.sidebar.multiselect("Filter", tree_df['caretaker'].unique())
+   st.sidebar.success("Select filter above.")
+   query = '(index == index or index != index)'
+   if owners != []:
+       query += ' and caretaker in @owners'
+   tree_df = tree_df.query(query)
+   df_dbh_grouped = pd.DataFrame(tree_df.groupby(['dbh']).count()['tree_id'])
+   df_dbh_grouped.columns = ['tree_count']
+   return df_dbh_grouped
 
-st.markdown('สวัสดี! *Streamlit*')
-st.title('Layout and Decoration')
-st.write("""
- เราจะลองทำ San Francisco Dataset กันดู
-""")
-st.divider()
 
-with st.spinner('Loading...'):
-    time.sleep(5)
-# st.success('Done!')
+# Define the pages
+def home_page():
+   st.markdown(f"# {list(pages.keys())[0]}")
+   st.title('San Francisco dataset')
+   st.write(""" Let's go """)
 
-#---------------Read data & filter-------------------------
-tree_df = pd.read_csv('trees.csv')
+   df_dbh_grouped = read_data()
+# Create column
+   col1, col2, col3 = st.columns(3)
+   with col1:
+       st.write('Column1')
+       st.line_chart(df_dbh_grouped)
+   with col2:
+       st.write('Column2')
+       st.bar_chart(df_dbh_grouped)
+   with col3:
+       st.write('Column3')
+       st.area_chart(df_dbh_grouped)
+   st.divider()
+def tab_page(tree_df):
+   st.markdown(f"# {list(pages.keys())[1]}")
+   st.title("About Page")
+   st.write("This is the about page of the app.")
 
-owners = st.sidebar.multiselect("Owner filter", tree_df['caretaker'].unique())
-st.sidebar.success("Select filter above.")
-query = '(index == index or index != index)'
+   owners = st.sidebar.multiselect("Filter", tree_df['caretaker'].unique())
+   st.sidebar.success("Select filter above.")
+   query = '(index == index or index != index)'
+   if owners != []:
+       query += ' and caretaker in @owners'
+   tree_df = tree_df.query(query)
+   df_dbh_grouped = pd.DataFrame(tree_df.groupby(['dbh']).count()['tree_id'])
+   df_dbh_grouped.columns = ['tree_count']
 
-if owners != []:
-    query += ' and caretaker in @owners'
+# Create tab
+   tab1, tab2, tab3 = st.tabs(["Line", "Bar", "Area"])
+   with tab1:
+       st.write('Column1')
+       st.line_chart(df_dbh_grouped)
+   with tab2:
+       st.write('Column2')
+       st.bar_chart(df_dbh_grouped)
+   with tab3:
+       st.write('Column3')
+       st.area_chart(df_dbh_grouped)
+   st.divider()
+def map_page():
+   st.title("Contact Page")
+   st.write("This is the contact page of the app.")
 
-tree_df = tree_df.query(query)
-df_dbh_grouped = pd.DataFrame(tree_df.groupby(['dbh']).count()['tree_id'])
-df_dbh_grouped.columns = ['tree_count']
-
-#---------------Create column-------------------------
-
-col1, col2, col3 = st.columns(3)
-with col1:
-    st.write('Column1')
-    st.line_chart(df_dbh_grouped)
-with col2:
-    st.write('Column2')
-    st.bar_chart(df_dbh_grouped)
-with col3:
-    st.write('Column3')
-    st.area_chart(df_dbh_grouped)
-st.divider()
+# Create a dictionary of pages
+pages = {
+   "Home": home_page,
+   "Tab": tab_page,
+   "Map": map_page
+}
+# Add a selectbox to the sidebar
+st.sidebar.title("Navigation")
+selection = st.sidebar.selectbox("Go to", list(pages.keys()))
+# Display the selected page
+page = pages[selection]
+page()
