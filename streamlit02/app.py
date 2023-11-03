a887e4be189c61e0319f46ceb13991915e805198
sutthipong_sangram <66056084@kmitl.ac.th>
2023-11-03 11:52:19 +0700

    streamlit 02 change pages

--- streamlit02/app.py
+++ streamlit02/app.py
@@ -3,18 +3,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from st_pages import Page, show_pages, add_page_title
-import time
-
-with st.spinner('Wait for it...'):
-    time.sleep(5)
-st.success('Done!')
-st.button("Reload")
 
 # Defind
-def read_data():
-   tree_df = pd.read_csv('trees.csv')
-   owners = st.sidebar.multiselect("Filter", tree_df['caretaker'].unique())
-   st.sidebar.success("Select filter above.")
+def filter_data(tree_df):
+   owners = st.sidebar.multiselect("Owner Filter", tree_df['caretaker'].unique())
    query = '(index == index or index != index)'
    if owners != []:
        query += ' and caretaker in @owners'
@@ -29,22 +21,23 @@ show_pages([
         Page('../pages/tab.py', 'Tab', '📈'),
         Page('../pages/map.py', 'Map', '🌍')
           ])
-
 st.title('Home')
 st.write(""" Welcome to san francisco tree dataset """)
 st.divider()
 
-#---------------Create column-------------------------
+# Read data
+tree_df = pd.read_csv('trees.csv')
 
-df_dbh_grouped = read_data()
+#---------------Create column-------------------------
+result = filter_data(tree_df)
 col1, col2, col3 = st.columns(3)
 with col1:
     st.write('Column1')
-    st.line_chart(df_dbh_grouped)
+    st.line_chart(result)
 with col2:
     st.write('Column2')
-    st.bar_chart(df_dbh_grouped)
+    st.bar_chart(result)
 with col3:
     st.write('Column3')
-    st.area_chart(df_dbh_grouped)
+    st.area_chart(result)
 st.divider()
