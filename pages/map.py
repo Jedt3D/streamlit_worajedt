c34871c1a94b7e3c7a16d95c16bc76f5ff6fe446
sutthipong_sangram <66056084@kmitl.ac.th>
2023-11-03 11:19:12 +0700

    streamlit 02 change pages

--- pages/map.py
+++ pages/map.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-
 tree_df = pd.read_csv('../streamlit02/trees.csv')
 owners = st.sidebar.multiselect("Filter", tree_df['caretaker'].unique())
 st.sidebar.success("Select filter above.")
@@ -10,19 +9,10 @@ query = '(index == index or index != index)'
 if owners != []:
     query += ' and caretaker in @owners'
 tree_df = tree_df.query(query)
-df_dbh_grouped = pd.DataFrame(tree_df.groupby(['dbh']).count()['tree_id'])
-df_dbh_grouped.columns = ['tree_count']
+# df_dbh_grouped = pd.DataFrame(tree_df.groupby(['dbh']).count()['tree_id'])
+# df_dbh_grouped.columns = ['tree_count']
 
-# df_dbh_grouped = read_data()
-#
-# df_dbh_grouped1 = df_dbh_grouped.dropna()
-#
-# df1 = pd.DataFrame(df_dbh_grouped1,columns=['latitude', 'longitude'])
-# st.map(df1)
-
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-df1 = pd.DataFrame(df_dbh_grouped,columns=['latitude', 'longitude'])
+tree_df1 = tree_df.replace('',np.nan)
+tree_df1 = tree_df1.dropna(axis=0)
+df1 = pd.DataFrame(tree_df1,columns=['latitude', 'longitude'])
 st.map(df1)
